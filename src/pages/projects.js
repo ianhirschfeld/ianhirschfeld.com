11a056e2bf6af09ae1af0a8e0323f282569ff2f8
Ian Hirschfeld <ianh7976@gmail.com>
2021-01-02 22:26:35 -0800

    Adds Atlas, CED, and GOOD Fridays projects.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,10 +10,19 @@ import SEO from '../components/seo'
 const ProjectsPage = () => {
   const data = useStaticQuery(graphql`
     query {
+      atlasImage: file(relativePath: { eq: "projects/Atlas.jpg" }) {
+        ...jpgImage
+      }
       balancesImage: file(relativePath: { eq: "projects/Balances.jpg" }) {
         ...jpgImage
       }
       beyondtokyoImage: file(relativePath: { eq: "projects/BeyondTokyo.jpg" }) {
+        ...jpgImage
+      }
+      cedImage: file(relativePath: { eq: "projects/CED.jpg" }) {
+        ...jpgImage
+      }
+      goodFridaysImage: file(relativePath: { eq: "projects/GoodFridays.jpg" }) {
         ...jpgImage
       }
       hackdesignImage: file(relativePath: { eq: "projects/HackDesign.png" }) {
@@ -35,37 +44,56 @@ const ProjectsPage = () => {
     {
       title: 'Role Classic',
       description:
-        'A role-playing party game built for your phone. Designed around the question: "What if D&D was as easy to play as Apples to Apples?" With no social media and $0 in marketing, Role Classic gained over 100,000 players.',
+        'Role-playing party game built for your phone. Designed around the question: "What if D&D was as easy to play as Apples to Apples?" With no social media and $0 in marketing, Role Classic gained over 100,000 players.',
       image: data.roleClassicImage.childImageSharp.fluid,
       url: 'https://apps.apple.com/us/app/role-classic/id933599454',
     },
     {
       title: 'Hack Design',
       description:
-        'An introductory product design course. With lessons written by industry professionals, Hack Design has more than 350,000 students.',
+        'Introductory product design course. With lessons written by industry professionals, Hack Design has more than 350,000 students.',
       image: data.hackdesignImage.childImageSharp.fluid,
       url: 'https://hackdesign.org',
     },
     {
       title: 'Beyond Tokyo',
       description:
-        'A roomscale VR travel experience built for the HTC Vive. Explore fantasy versions of famous locations in Japan while taking a journey through an interactive story about Hachiko, Japan’s most loyal dog.',
+        'Roomscale VR travel experience built for the HTC Vive. Explore fantasy versions of famous locations in Japan while taking a journey through an interactive story about Hachiko, Japan’s most loyal dog.',
       image: data.beyondtokyoImage.childImageSharp.fluid,
       url: 'https://www.beyondtokyovr.com',
     },
     {
       title: 'Never Bout Us',
       description:
-        "A ground breaking 360 music video created for VR viewing. Released in 2016, it went on to gain over 800,000 views. It's release was accompanied by a physical press kit including a custom cardbord VR viewer and memorabilia from the artist.",
+        "Ground breaking 360 music video created for VR viewing. Released in 2016, it went on to gain over 800,000 views. It's release was accompanied by a physical press kit including a custom cardbord VR viewer and memorabilia from the artist.",
       image: data.neverBoutUsImage.childImageSharp.fluid,
       url: 'https://www.youtube.com/watch?v=_dw-Q6seOOk',
     },
     {
+      title: 'Atlas',
+      description:
+        'VR sci-fi adventure for the Oculus Rift & Touch. Explore the world on your hover bike and solve environmental puzzles to unlock the mysteries hiding within the world.',
+      image: data.atlasImage.childImageSharp.fluid,
+      url: 'https://vimeo.com/205636652',
+    },
+    {
       title: 'Balances',
       description:
-        'A Bitcoin financial app. Send & receive payments and track the balances of all your cryptocurrencies.',
+        'Bitcoin financial app. Send & receive payments and track the balances of all your cryptocurrencies.',
       image: data.balancesImage.childImageSharp.fluid,
       url: 'https://web.archive.org/web/20170709185626/https://balances.io/',
+    },
+    {
+      title: 'CED Innovators',
+      description: 'Interactive data visualization. Created with D3.js.',
+      image: data.cedImage.childImageSharp.fluid,
+      url: 'https://cednc.org/innovatorsreport/',
+    },
+    {
+      title: 'GOOD Fridays',
+      description: 'iOS music player. An exploration in mobile audio player design with the music of Kanye West.',
+      image: data.goodFridaysImage.childImageSharp.fluid,
+      url: 'https://apps.apple.com/us/app/good-fridays-kanye-west-edition/id1078562945',
     },
     {
       title: 'Retina Mac Apps',
