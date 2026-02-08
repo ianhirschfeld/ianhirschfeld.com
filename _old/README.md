3bbb3ca47717f755c3f03f3eaf29c1434b6d2030
Ian Hirschfeld <ian.n.hirschfeld@gmail.com>
2026-02-07 20:17:49 -0800

    removes old gatsby site code

--- _old/README.md
+++
@@ -1,1 +0,0 @@
-This is my personal website. Check it out to learn about what I'm working on and view my past work.
--- _old/src/utilities.js
+++
@@ -1,3 +0,0 @@
-export const randomIntBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-
-export const randomMultiplier = () => (Math.random() < 0.5 ? -1 : 1)
